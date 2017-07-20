2a52b8c26510e49979a77b08f19b402260912dbc
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-20 10:47:11 -0600

    Add missing information to Version class

--- src/main/kotlin/com/lapismc/minecraft/versioning/Version.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/Version.kt
@@ -2,19 +2,47 @@ package com.lapismc.minecraft.versioning
 
 /**
  * Complete information about a version of the game and its dependencies.
+ * @param stub Basic information about the version.
  * @param assetIndex Summary of assets needed for the game and where to find them.
  * @param downloads Logical downloads for items such as the client and server.
  * @param libraries Packages the game depends on to run.
  * @param launcher Information needed for the launcher to start the game.
  */
-class Version(val assetIndex: AssetIndex, val downloads: List<Resource>,
+class Version(stub: VersionStub, val assetIndex: AssetIndex, val downloads: List<Resource>,
               val libraries: List<Library>, val launcher: LauncherInfo) {
+    /**
+     * Unique name of the version.
+     */
+    val id = stub.id
+
+    /**
+     * Type of version (release, snapshot, etc.).
+     */
+    val type = stub.type
+
+    /**
+     * Last date and time the version was updated.
+     * May be newer than the release time.
+     */
+    val updateTime = stub.updateTime
+
+    /**
+     * Date and time the version was first released.
+     */
+    val releaseTime = stub.releaseTime
+
+    /**
+     * Location of where to get additional information for the version.
+     */
+    val url = stub.url
+
     /**
      * Constructs information for a single version of the game.
      * @param assetIndex Summary of assets needed for the game and where to find them.
      * @param launcher Information needed for the launcher to start the game.
      */
-    class Builder(val assetIndex: AssetIndex, val launcher: LauncherInfo) {
+    class Builder(private val stub: VersionStub, private val assetIndex: AssetIndex,
+                  private val launcher: LauncherInfo) {
         private val downloads = ArrayList<Resource>()
         private val libraries = ArrayList<Library>()
 
@@ -42,7 +70,7 @@ class Version(val assetIndex: AssetIndex, val downloads: List<Resource>,
          * @return Constructed version information.
          */
         fun build(): Version {
-            return Version(assetIndex, downloads.toList(), libraries.toList(), launcher)
+            return Version(stub, assetIndex, downloads.toList(), libraries.toList(), launcher)
         }
     }
 }
