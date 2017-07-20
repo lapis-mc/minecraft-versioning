96477f5313a9c76ceeb8613e835a33563bb301d5
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-20 12:45:03 -0600

    Move OS detection logic to OSType
    Add ability to get enum element for the current OS.
    This simplifies things.

--- src/main/kotlin/com/lapismc/minecraft/versioning/OSRule.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/OSRule.kt
@@ -6,38 +6,9 @@ package com.lapismc.minecraft.versioning
  * @param allowed Flag indicating whether the library should be included.
  */
 class OSRule(val os: OSType, allowed: Boolean) : Rule(allowed) {
-    companion object {
-        /**
-         * Name of the current operating system.
-         */
-        private val localOSName = System.getProperty("os.name").toLowerCase()
-
-        /**
-         * Checks whether the local system appears to be Windows.
-         * @return True if the local OS is Windows, false otherwise.
-         */
-        fun isWindows() = localOSName.indexOf("win") >= 0
-
-        /**
-         * Checks whether the local system appears to be Mac OSX.
-         * @return True if the local OS is Mac OSX, false otherwise.
-         */
-        fun isOSX() = localOSName.indexOf("mac") >= 0
-
-        /**
-         * Checks whether the local system appears to be Linux.
-         * @return True if the local OS is Linux, false otherwise.
-         */
-        fun isLinux() = localOSName.indexOf("linux") >= 0
-    }
-
     /**
      * Checks whether this rule applies to the current system.
      * @return True if the rule should be considered, false otherwise.
      */
-    override fun isApplicable(): Boolean = when(os) {
-        OSType.WINDOWS -> isWindows()
-        OSType.OSX     -> isOSX()
-        OSType.LINUX   -> isLinux()
-    }
+    override fun isApplicable(): Boolean = OSType.current == os
 }
--- src/main/kotlin/com/lapismc/minecraft/versioning/OSType.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/OSType.kt
@@ -17,5 +17,41 @@ enum class OSType {
     /**
      * Anything not Windows or OSX.
      */
-    LINUX
+    LINUX;
+
+    companion object {
+        /**
+         * Name of the current operating system.
+         */
+        private val localOSName = System.getProperty("os.name").toLowerCase()
+
+        /**
+         * Checks whether the local system appears to be Windows.
+         * @return True if the local OS is Windows, false otherwise.
+         */
+        fun isWindows() = localOSName.indexOf("win") >= 0
+
+        /**
+         * Checks whether the local system appears to be Mac OSX.
+         * @return True if the local OS is Mac OSX, false otherwise.
+         */
+        fun isOSX() = localOSName.indexOf("mac") >= 0
+
+        /**
+         * Checks whether the local system appears to be Linux.
+         * @return True if the local OS is Linux, false otherwise.
+         */
+        fun isLinux() = localOSName.indexOf("linux") >= 0
+
+        /**
+         * Gets the enumeration element representative of the current operating system.
+         * @return Element representing the current OS.
+         */
+        val current = when {
+            isWindows() -> WINDOWS
+            isOSX()     -> OSX
+            isLinux()   -> LINUX
+            else        -> TODO()
+        }
+    }
 }
