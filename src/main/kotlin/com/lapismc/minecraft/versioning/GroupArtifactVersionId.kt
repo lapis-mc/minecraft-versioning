51678076a2f78be7747f60156e235e24c822998e
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-20 15:06:54 -0600

    Add parsing of GAV

--- src/main/kotlin/com/lapismc/minecraft/versioning/GroupArtifactVersionId.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/GroupArtifactVersionId.kt
@@ -12,4 +12,17 @@ data class GroupArtifactVersionId(val groupId: String, val artifactId: String, v
      * @return Group, artifact, and version separated by colons.
      */
     override fun toString(): String = "$groupId:$artifactId:$version"
+
+    companion object {
+        /**
+         * Extracts GAV information from a formatted string.
+         * @param gav String containing the GAV information.
+         *  This format should be GROUP:ARTIFACT:VERSION
+         * @return Extract GAV information.
+         */
+        fun parse(gav: String): GroupArtifactVersionId {
+            val (groupId, artifactId, version) = gav.split(':', limit = 3)
+            return GroupArtifactVersionId(groupId, artifactId, version)
+        }
+    }
 }
