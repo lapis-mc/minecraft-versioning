78971ee4bc3f3986e32316504ac4d28cebf7f062
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-28 10:18:42 -0600

    Return result wrapper

--- src/main/kotlin/com/lapismc/minecraft/versioning/MetaService.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/MetaService.kt
@@ -1,26 +1,34 @@
 package com.lapismc.minecraft.versioning
 
+import com.github.kittinunf.result.Result
+
 /**
  * Provides methods for retrieving version information from a meta-service.
  */
 interface MetaService {
     /**
      * Retrieves summarized information about all available versions.
-     * @return Top-level version information.
+     * @return Result of the request.
+     *  If the request was successful, then the top-level version information (stubs) are returned.
+     *  If the request failed, then the exception information is returned.
      */
-    fun getVersionManifest(): VersionManifest
+    fun getVersionManifest(): Result<VersionManifest, Exception>
 
     /**
      * Retrieves additional information for a version.
      * @param stub Version information from the manifest.
-     * @return Complete version information referenced by the stub.
+     * @return Result of the request.
+     *  If the request was successful, then the complete version information referenced by the stub is returned.
+     *  If the request failed, then the exception information is returned.
      */
-    fun getVersion(stub: VersionStub): Version
+    fun getVersion(stub: VersionStub): Result<Version, Exception>
 
     /**
      * Retrieves a list of assets needed for the game to run.
      * @param index Reference to the list of assets to retrieve.
-     * @return List of assets corresponding to the specified index.
+     * @return Result of the request.
+     *  If the request was successful, then a list of assets corresponding to the specified index is returned.
+     *  If the request failed, then the exception information is returned.
      */
-    fun getAssetList(index: AssetIndex): AssetList
+    fun getAssetList(index: AssetIndex): Result<AssetList, Exception>
 }
--- src/main/kotlin/com/lapismc/minecraft/versioning/WebMetaService.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/WebMetaService.kt
@@ -1,6 +1,7 @@
 package com.lapismc.minecraft.versioning
 
 import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
 import com.lapismc.minecraft.versioning.serialization.*
 
 /**
@@ -20,42 +21,36 @@ class WebMetaService(private val manifestUrl: String) : MetaService {
 
     /**
      * Retrieves summarized information about all available versions.
-     * @return Top-level version information.
+     * @return Result of the request.
+     *  If the request was successful, then the top-level version information (stubs) are returned.
+     *  If the request failed, then the exception information is returned.
      */
-    override fun getVersionManifest(): VersionManifest {
+    override fun getVersionManifest(): Result<VersionManifest, Exception> {
         val (_, _, result) = Fuel.get(manifestUrl).responseObject(VersionManifestJsonDeserializer())
-        result.fold(success = {
-            return result.get()
-        }, failure = {
-            TODO()
-        })
+        return result
     }
 
     /**
      * Retrieves additional information for a version.
      * @param stub Version information from the manifest.
-     * @return Complete version information referenced by the stub.
+     * @return Result of the request.
+     *  If the request was successful, then the complete version information referenced by the stub is returned.
+     *  If the request failed, then the exception information is returned.
      */
-    override fun getVersion(stub: VersionStub): Version {
+    override fun getVersion(stub: VersionStub): Result<Version, Exception> {
         val (_, _, result) = Fuel.get(stub.url).responseObject(VersionJsonDeserializer(stub.url))
-        result.fold(success = {
-            return result.get()
-        }, failure = {
-            TODO()
-        })
+        return result
     }
 
     /**
      * Retrieves a list of assets needed for the game to run.
      * @param index Reference to the list of assets to retrieve.
-     * @return List of assets corresponding to the specified index.
+     * @return Result of the request.
+     *  If the request was successful, then a list of assets corresponding to the specified index is returned.
+     *  If the request failed, then the exception information is returned.
      */
-    override fun getAssetList(index: AssetIndex): AssetList {
+    override fun getAssetList(index: AssetIndex): Result<AssetList, Exception> {
         val (_, _, result) = Fuel.get(index.resource.url).responseObject(AssetListJsonDeserializer())
-        result.fold(success = {
-            return result.get()
-        }, failure = {
-            TODO()
-        })
+        return result
     }
 }
