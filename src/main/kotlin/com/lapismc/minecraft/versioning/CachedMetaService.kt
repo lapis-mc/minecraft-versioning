65f22127aff2dc4fdc52b689f96ff1f1abfa2049
Michael Miller <icy.arctic.fox@gmail.com>
2017-08-01 12:57:13 -0600

    Make hash map access generic

--- src/main/kotlin/com/lapismc/minecraft/versioning/CachedMetaService.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/CachedMetaService.kt
@@ -36,15 +36,9 @@ class CachedMetaService(private val service: MetaService) : MetaService {
      */
     override fun getVersion(stub: VersionStub): Result<Version, Exception> {
         val key = stub.id
-        val version = versionCache[key]
-        return if(version != null)
-            Result.of(version)
-        else {
-            val result = service.getVersion(stub)
-            if(result is Result.Success)
-                versionCache[key] = result.value
-            result
-        }
+        return cachedAccess(key, versionCache, {
+            service.getVersion(stub)
+        })
     }
 
     /**
@@ -54,15 +48,9 @@ class CachedMetaService(private val service: MetaService) : MetaService {
      */
     override fun getAssetList(index: AssetIndex): Result<AssetList, Exception> {
         val key = index.resource.name
-        val assetList = assetListCache[key]
-        return if(assetList != null)
-            Result.of(assetList)
-        else {
-            val result = service.getAssetList(index)
-            if(result is Result.Success)
-                assetListCache[key] = result.value
-            result
-        }
+        return cachedAccess(key, assetListCache, {
+            service.getAssetList(index)
+        })
     }
 
     /**
@@ -73,5 +61,29 @@ class CachedMetaService(private val service: MetaService) : MetaService {
         cachedManifest = null
         versionCache.clear()
         assetListCache.clear()
+    }
+
+    /**
+     * Generic method for accessing caches.
+     * If a value is already in the cache, this method will return it.
+     * If not, then the [missing] block is called to retrieve the value.
+     * @param key Unique key used to address the value in the cache.
+     * @param map Hash map representing the cache.
+     * @param missing Block to call to get the value when there's a cache miss.
+     *  The block is expected to return a result.
+     *  If the result is successful, then the value is stored in the cache.
+     * @return Result of retrieving a value from the cache.
+     *  If the item is not in the cache and can't be retrieved, an error is returned.
+     */
+    private fun <T : Any> cachedAccess(key: String, map: HashMap<String, T>, missing: () -> Result<T, Exception>): Result<T, Exception> {
+        val cachedValue = map[key]
+        return if(cachedValue != null)
+            Result.of(cachedValue)
+        else {
+            val result = missing.invoke()
+            if(result is Result.Success)
+                map[key] = result.value
+            result
+        }
     }
 }
