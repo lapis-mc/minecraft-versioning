ae830ed72abad3800f56ffb10dd7ea73d595ca1a
Michael Miller <icy.arctic.fox@gmail.com>
2017-08-01 13:29:21 -0600

    Implement download methods for web meta-service

--- src/main/kotlin/com/lapismc/minecraft/versioning/WebMetaService.kt
+++ src/main/kotlin/com/lapismc/minecraft/versioning/WebMetaService.kt
@@ -7,16 +7,19 @@ import com.lapismc.minecraft.versioning.serialization.*
 /**
  * Web-based meta-service.
  * Retrieves version information from the web.
+ * @param manifestUrl URL of where the version manifest document can be retrieved from.
+ * @param resourceUrl Base URL of where the game assets can be downloaded from.
  */
-class WebMetaService(private val manifestUrl: String) : MetaService {
+class WebMetaService(private val manifestUrl: String, private val resourceUrl: String) : MetaService {
     companion object {
         private const val officialManifestUrl = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+        private const val officialResourceUrl = "https://resources.download.minecraft.net"
 
         /**
          * Gets a meta-service pointing to the official Mojang version manifest.
          * @return Official meta-service.
          */
-        fun official() = WebMetaService(officialManifestUrl)
+        fun official() = WebMetaService(officialManifestUrl, officialResourceUrl)
     }
 
     /**
@@ -50,7 +53,10 @@ class WebMetaService(private val manifestUrl: String) : MetaService {
      *  If the request was successful, then version information document referenced by the stub is returned.
      *  If the request failed, then the exception information is returned.
      */
-    override fun getVersionDocument(stub: VersionStub): Result<ByteArray, Exception> = TODO()
+    override fun getVersionDocument(stub: VersionStub): Result<ByteArray, Exception> {
+        val (_, _, result) = Fuel.get(stub.url).response()
+        return result
+    }
 
     /**
      * Retrieves a list of assets needed for the game to run.
@@ -72,7 +78,10 @@ class WebMetaService(private val manifestUrl: String) : MetaService {
      *  If the request was successful, then the asset list document corresponding to the specified index is returned.
      *  If the request failed, then the exception information is returned.
      */
-    override fun getAssetListDocument(index: AssetIndex): Result<ByteArray, Exception> = TODO()
+    override fun getAssetListDocument(index: AssetIndex): Result<ByteArray, Exception> {
+        val (_, _, result) = Fuel.get(index.resource.url).response()
+        return result
+    }
 
     /**
      * Retrieves the raw content of an asset.
@@ -82,7 +91,11 @@ class WebMetaService(private val manifestUrl: String) : MetaService {
      *  If the request was successful, then the asset content is returned.
      *  If the request failed, then the exception information is returned.
      */
-    override fun getAssetContent(asset: Asset): Result<ByteArray, Exception> = TODO()
+    override fun getAssetContent(asset: Asset): Result<ByteArray, Exception> {
+        val url = constructAssetUrl(asset)
+        val (_, _, result) = Fuel.get(url).response()
+        return result
+    }
 
     /**
      * Retrieves the raw content of an artifact from a library.
@@ -92,5 +105,19 @@ class WebMetaService(private val manifestUrl: String) : MetaService {
      *  If the request was successful, then the artifact content is returned.
      *  If the request failed, then the exception information is returned.
      */
-    override fun getArtifactContent(artifact: Artifact): Result<ByteArray, Exception> = TODO()
+    override fun getArtifactContent(artifact: Artifact): Result<ByteArray, Exception> {
+        val (_, _, result) = Fuel.get(artifact.resource.url).response()
+        return result
+    }
+
+    /**
+     * Creates a URL indicating where an asset can be downloaded from.
+     * @param asset Reference to the asset to generate a URL for.
+     * @return URL of the asset.
+     */
+    private fun constructAssetUrl(asset: Asset): String {
+        val hash = asset.hash
+        val dir  = hash.substring(0..2)
+        return "$resourceUrl/$dir/$hash"
+    }
 }
